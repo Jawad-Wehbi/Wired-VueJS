8e9cc9ce06635578d4ecd6b715c7450b3e528bfc
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2023-03-06 16:34:18 +0200

    Creating Navigation Bar.

---
+++ src/components/NavBar/NavigationBar.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  data() {
+    return {
+    };
+  },
+});
+</script>
+
+<template>
+<nav class="navbar">
+
+</nav>
+</template>
+
+<style lang="scss" src="./NavigationBar.scss"></style>
