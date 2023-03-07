572d6ad13ef63f84b3e7bd2063c1909ee6d593b5
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2023-03-07 09:37:39 +0200

    Modifying Navigation Bar.

--- src/components/NavBar/NavigationBar.vue
+++ src/components/NavBar/NavigationBar.vue
@@ -1,17 +1,32 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+
 export default defineComponent({
   data() {
-    return {
-    };
+    return {};
   },
 });
 </script>
 
 <template>
-<nav class="navbar">
-
-</nav>
+  <nav class="navbar">
+    <div class="title inline-block">
+      <span><img class="logo" src="../../assets/logo.png" /></span>
+      <span class="wired-font">WIRED</span>
+    </div>
+    <div class="right-sec">
+      <button class="edit-name">
+        <v-icon class="white" icon="mdi-account-circle"></v-icon>
+      </button>
+      <div class="white profile-name">Jawad Wehbi</div>
+      <div class="white ">
+        <button class="sign-out">
+          <v-icon class=" sign-out-icon" icon="mdi-logout"></v-icon>
+          SIGNOUT
+        </button>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <style lang="scss" src="./NavigationBar.scss"></style>
