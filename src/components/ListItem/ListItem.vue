794df9613baac970423acb3878c6531e21f86c7a
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2023-03-08 17:08:04 +0200

    Implementing List Item

---
+++ src/components/ListItem/ListItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="item-list">
+    <div class="left-sec">
+      <div class="member-name">FullName</div>
+      <div class="working-task">Working on Project - Category</div>
+    </div>
+    <div class="right-sec">00:00:00</div>
+  </div>
+</template>
+
+<script lang="ts" src="./ListItem"></script>
+
+<style lang="scss" src="./ListItem.scss"></style>
