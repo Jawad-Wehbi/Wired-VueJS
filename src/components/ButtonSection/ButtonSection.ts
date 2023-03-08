445d4252f7aca63b7ad848f6bacd6f1518054443
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2023-03-08 17:07:25 +0200

    Creating Buttons Section

---
+++ src/components/ButtonSection/ButtonSection.ts
@@ -0,0 +1,8 @@
+import { defineComponent } from 'vue';
+import MainButton from '../MainButton/MainButton.vue';
+export default defineComponent({
+  data() {
+    return {};
+  },
+  components: { MainButton },
+});
---
+++ src/components/ButtonSection/ButtonSection.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="buttons-container">
+    <MainButton
+      icon="mdi-plus"
+      buttonName="ADD SHORTCUT"
+      :styles="{ color: '#F51B5', backgroundColor: 'white' }"
+    />
+    <MainButton
+      icon="mdi-checkbox-marked"
+      buttonName="SHOW ALL TASKS"
+      :styles="{ color: 'white', backgroundColor: '#1976d2' }"
+    />
+    <MainButton
+      icon="mdi-stop"
+      buttonName="END ALL TASKS"
+      :styles="{ color: 'white', backgroundColor: '#1976d2' }"
+    />
+    <MainButton
+      icon="mdi-star"
+      buttonName="CUSTOM TASKS"
+      :styles="{ color: 'white', backgroundColor: '#1976d2' }"
+    />
+  </div>
+</template>
+
+<script lang="ts" src="./ButtonSection.ts"></script>
+
+<style lang="scss" src="./ButtonSection.scss" scoped></style>
