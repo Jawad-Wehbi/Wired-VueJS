bcb043c7491e062cc80d977b0680babe5d671189
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2023-03-08 11:43:23 +0200

    Creating the Timer Component.

---
+++ src/components/TimerComponent/TimerComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="timer-container">
+    <div>
+      <v-icon icon="mdi-laptop"></v-icon>
+    </div>
+    <div>Total Work Time</div>
+    <div>00:00:00</div>
+  </div>
+</template>
+
+<script lang="ts" src="./TimerComponent"></script>
+
+<style lang="scss" src="./TimerComponent.scss"></style>
