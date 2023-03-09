aaacd1baedcccd32ba8c57016faa211460bd2ad0
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2023-03-09 16:28:59 +0200

    File Structuring

---
+++ src/views/LoginView/LoginView.ts
@@ -0,0 +1,45 @@
+import { defineComponent } from 'vue';
+import axios from 'axios';
+import InputComponent from '@/components/InputComponent/InputComponent.vue';
+export default defineComponent({
+  data() {
+    return {
+      Page: 'Login Page!',
+      email: '',
+      domain: '',
+      error: '',
+    };
+  },
+  components: { InputComponent },
+  methods: {
+    async login() {
+      try {
+        const response = await axios.post(
+          'https://api.wired.sowlutions.com/api/v2/signin',
+          {
+            email: this.email,
+            domain: this.domain,
+          }
+        );
+        console.log(response.data);
+        // Route to the next page if login is successful
+        this.$router.push('/verify-login');
+      } catch (error) {
+        console.error(error);
+        this.error = 'Invalid email or domain';
+      }
+    },
+    autofillDomain(): void {
+      const atIndex = this.email.indexOf('@');
+      if (atIndex !== -1) {
+        this.domain = this.email.slice(atIndex + 1);
+      }
+    },
+  },
+  computed: {
+    isValidEmail(): boolean {
+      const emailRegex = /\S+@\S+\.\S+/;
+      return emailRegex.test(this.email);
+    },
+  },
+});
--- src/views/VerifyLogin/VerifyLogin.vue
+++ src/views/VerifyLogin/VerifyLogin.vue
@@ -24,50 +24,7 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import axios from 'axios';
-
-export default defineComponent({
-  data() {
-    return {
-      Page: 'Verify Login Page!',
-      auth_token: '',
-      error: '',
-    };
-  },
-  methods: {
-    async verify() {
-      try {
-        const response = await axios.get(
-          'https://api.wired.sowlutions.com/api/v2/users/info',
-          {
-            headers: {
-              Authorization: this.auth_token,
-            },
-          }
-        );
-        console.log(response.data);
-        // Save data to local storage
-        localStorage.setItem('user', JSON.stringify(response.data));
-
-        // Save auth token to cookies
-        // this.$cookies.set("Authorization", this.auth_token);
-
-        // Navigate to home page
-        this.$router.push('/');
-      } catch (error) {
-        console.error(error);
-        this.error = 'Invalid email or domain';
-      }
-    },
-  },
-  computed: {
-    isAuthTokenValid(): boolean {
-      return this.auth_token.length === 32;
-    },
-  },
-});
+<script lang="ts" src="./VerifyLogin">
 </script>
 
 <style lang="scss" src="../LoginView/LoginView.scss"></style>
