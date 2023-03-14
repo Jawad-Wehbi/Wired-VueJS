8c549d7f11f09e5a85050aa704627ca98b5c3ed5
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2023-03-14 19:35:10 +0200

    Implementing the Functionality of Team Plans.

--- src/components/MembersBoard/MembersBoard.ts
+++ src/components/MembersBoard/MembersBoard.ts
@@ -1,13 +1,17 @@
 import { defineComponent, PropType } from 'vue';
 import ListItem from '../ListItem/ListItem.vue';
 import ListButtons from '../ListButtons/ListButtons.vue';
+import TaskCard from '../TaskCard/TaskCard.vue';
 import ButtonSection from '../ButtonSection/ButtonSection.vue';
 import { TaskRecord } from 'DataTypes';
+import { isSameDay, parseISO } from 'date-fns';
 export default defineComponent({
   data() {
     return {
       plansDialog: false,
       activeMembers: [] as TaskRecord[],
+      teamPlans: [] as TaskRecord[],
+      plansArray: [] as TaskRecord[],
     };
   },
   methods: {
@@ -35,25 +39,46 @@ export default defineComponent({
   mounted() {
     setTimeout(() => {
       this.reduce();
+      console.log(this.plansArray);
     }, 3000);
   },
-  // computed: {
-  //   todayData(): TaskRecord[] {
-  //     const today = new Date();
-  //     console.log(today);
-  //     return this.MembersInfo.filter((items) => {
-  //       const itemDate = parseISO(items.first_log.start_date);
-  //       return isSameDay(today, itemDate);
-  //     }).filter((items) => {
-  //       return items.status === 'started' || items.status === 'paused';
-  //     });
-  //   },
-  // },
+  computed: {
+    todayData(): TaskRecord[] {
+      const today = new Date();
+      console.log(today);
+      return this.MembersInfo.filter((items) => {
+        const itemDate = parseISO(items.first_log.start_date);
+        return isSameDay(today, itemDate);
+      });
+      // .filter((items) => {
+      //       return items.status === 'started' || items.status === 'paused';
+      //     });
+    },
+    plansData(): TaskRecord[] {
+      console.log('CHECK ACTIVEMEMBERS', this.MembersInfo);
+      const planningArray = [] as TaskRecord[];
+      const today = new Date();
+      return this.MembersInfo.filter((items) => {
+        const itemDate = parseISO(items.first_log.start_date);
+        return isSameDay(today, itemDate);
+      }).filter((teamPlans) => {
+        if (
+          teamPlans.project.name === 'Internal' &&
+          teamPlans.task_category.name === 'Planning'
+        ) {
+          planningArray.push(teamPlans);
+          return planningArray;
+        }
+        console.log('Team planningArray', planningArray);
+        this.plansArray = planningArray;
+      });
+    },
+  },
   props: {
     MembersInfo: {
       type: Array as PropType<TaskRecord[]>,
       required: true,
     },
   },
-  components: { ListItem, ListButtons, ButtonSection },
+  components: { ListItem, ListButtons, ButtonSection, TaskCard },
 });
--- src/components/MembersBoard/MembersBoard.vue
+++ src/components/MembersBoard/MembersBoard.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="active-members">
     <div class="members-title">
-      <v-icon icon="mdi-account-multiple" size="small"></v-icon>
+      <v-icon
+        icon="mdi-account-multiple"
+        size="small"
+      ></v-icon>
       <div class="my-tasks">Active members</div>
     </div>
     <div class="members-container">
       <ListItem
-        @click=""
         @togglelistItem="passUserId"
         v-for="item in activeMembers"
         :MembersInfo="item"
@@ -14,31 +16,47 @@
       />
     </div>
     <div class="list-bottom">
-      <ListButtons listIcon="mdi-refresh" listButtonName="RELOAD" />
+      <ListButtons
+        listIcon="mdi-refresh"
+        listButtonName="RELOAD"
+      />
       <ListButtons
         listIcon="mdi-account-details"
         listButtonName="DETAILS"
         @click="toggleDetails"
       />
-      <ListButtons listIcon="mdi-notebook" listButtonName="PLANS" />
-      <v-dialog v-model="plansDialog" width="auto" center>
-        <div class="all-tasks-overlay">
-          <div class="overlay-title">
-            <div>
-              <v-icon icon="mdi-laptop" size="small"></v-icon>
-              <div class="all-tasks">All Tasks</div>
+      <ListButtons
+        listIcon="mdi-notebook"
+        listButtonName="PLANS"
+        @click="plansDialog=true"
+      />
+      <v-dialog
+        v-model="plansDialog"
+        width="auto"
+        center
+      >
+        <div class="plans-overlay">
+          <div class="plans-title-section">
+            <div class="plans-title">
+              <v-icon
+                icon="mdi-notebook"
+                size="small"
+              ></v-icon>
+              <div class="team-plans">Team's Plans</div>
             </div>
             <div>
-              <v-icon icon="mdi-close" size="small"></v-icon>
+              <v-icon
+                icon="mdi-close"
+                size="small"
+              ></v-icon>
             </div>
           </div>
           <div class="tasks-list">
-            <TaskCard />
-            <TaskCard />
-            <TaskCard />
-            <TaskCard />
-            <TaskCard />
-            <TaskCard />
+            <TaskCard
+              v-for="item in plansData"
+              :TaskDetails="item"
+              :key="item.id"
+            />
           </div>
         </div>
       </v-dialog>
