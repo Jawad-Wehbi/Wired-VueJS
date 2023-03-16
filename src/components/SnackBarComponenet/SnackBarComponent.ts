2b52169fe0ee185533e493675d32a782105ced07
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2023-03-16 23:28:34 +0200

    Implementing the Snack Bar Component.

---
+++ src/components/SnackBarComponenet/SnackBarComponent.ts
@@ -0,0 +1,8 @@
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data: () => ({
+    snackbar: false,
+    text: `Hello, I'm a snackbar`,
+  }),
+});
---
+++ src/components/SnackBarComponenet/SnackBarComponent.vue
@@ -0,0 +1,13 @@
+<script lang="ts" src="./SnackBarComponent"></script>
+
+<template>
+  <v-snackbar v-model="snackbar">
+    {{ text }}
+
+    <template v-slot:actions>
+      <v-btn color="pink" variant="text" @click="snackbar = false">
+        Close
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
