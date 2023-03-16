10572a8035eae40d574f6714918f6ab18b8d44da
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2023-03-16 23:18:51 +0200

    Implementing Buttons Functionality

--- src/components/ButtonSection/ButtonSection.ts
+++ src/components/ButtonSection/ButtonSection.ts
@@ -1,12 +1,25 @@
+import { get, post, put } from '@/service';
+import { projectInfo, TaskRecord } from 'DataTypes';
 import { defineComponent } from 'vue';
 import MainButton from '../MainButton/MainButton.vue';
 import TaskCard from '../TaskCard/TaskCard.vue';
 import NewTaskCard from '../NewTaskCard/NewTaskCard.vue';
+import { isSameDay, isToday, parseISO } from 'date-fns';
 export default defineComponent({
   data() {
     return {
+      selectedOption1: null,
+      projectCategoriesArray: {} as projectInfo,
+      result: [] as projectInfo[],
+      selectedProject: '',
+      selectedCategory: '',
+      projects: [] as string[],
+      categories: [] as string[],
       addShortcutDialog: false,
       allTasksDialog: false,
+      offset: 0,
+      limit: 12,
+      allTasks: [] as TaskRecord[],
       items: [
         { title: 'Click Me' },
         { title: 'Click Me' },
@@ -15,5 +28,180 @@ export default defineComponent({
       ],
     };
   },
+  computed: {
+    selectDisplay(): string {
+      return this.selectedProject ? 'block' : 'none';
+    },
+    isCategorySelected(): boolean {
+      return this.selectedCategory === 'false';
+    },
+  },
+  async mounted() {
+    try {
+      const response = await get('/team/projects');
+      this.result = response.data.data;
+      this.getNames(this.result);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  methods: {
+    async endAllTasks(id: number) {
+      try {
+        const getresponse = await get('/tasks');
+        const data = getresponse.data.data.items;
+        console.log('data :>> ', getresponse);
+        const filteredByToday: TaskRecord[] = data.filter(
+          (item: TaskRecord) => {
+            return isToday(parseISO(item.first_log.start_date));
+          }
+        );
+        console.log('filteredByToday :>> ', filteredByToday);
+        // const tasksIds = this.allTasks.map((data: TaskRecord) => data.id);
+        // console.log(tasksIds);
+        filteredByToday.forEach(async (kalb: TaskRecord) => {
+          try {
+            if (kalb.status !== 'finished') {
+              const response = await put(`/tasks/${kalb.id}/end`);
+              console.log('========>', response, kalb);
+              this.result = response.data.data;
+            }
+          } catch (error) {
+            console.error(error);
+          }
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async getAllTasks() {
+      try {
+        const response = await get('/tasks', {
+          offset: this.offset,
+          limit: this.limit,
+        });
+        console.log('========>', response.data);
+        const data = response.data.data.items;
+        this.offset = this.offset + this.limit;
+        this.allTasks.push(...data);
+        console.log(this.allTasks);
+        return (this.allTasksDialog = true);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async createNewShortcut() {
+      try {
+        const response = await post('/user_shortcuts', {
+          data: {
+            project_name: this.selectedProject,
+            category_name: this.selectedCategory,
+          },
+          shortcut_type: 'task_start',
+        });
+        this.result = response.data.data;
+        console.log('object');
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    getNames(arr: projectInfo[]) {
+      return (this.projects = arr.map((obj) => obj.name));
+    },
+    getProjectInfo(): void {
+      let newArray: projectInfo[] = [];
+      newArray = this.result.filter((obj) => {
+        return obj.name === this.selectedProject;
+      });
+      this.projectCategoriesArray = newArray[0];
+      console.log('SELECTED PROJECT OBJ', this.projectCategoriesArray);
+      this.getProjectCategories(this.projectCategoriesArray);
+    },
+    getProjectCategories(obj: projectInfo): string[] {
+      return (this.categories = obj.task_categories.map((obj) => obj.name));
+    },
+  },
+  watch: {
+    selectedProject() {
+      this.getProjectInfo();
+    },
+  },
   components: { MainButton, TaskCard, NewTaskCard },
 });
+function data(
+  value: {
+    id: number;
+    due_date: string;
+    estimated_time_hours: number;
+    estimated_time_minutes: number;
+    total_spent_time: string;
+    notes: string;
+    status: string;
+    progress: number;
+    extra_data: string;
+    project: { id: number; name: string; deletable: boolean };
+    task_category: { id: number; name: string; deletable: boolean };
+    first_log: {
+      id: number;
+      start_date: string;
+      end_date?: string | undefined;
+      difference?: number | undefined;
+    };
+    last_log: {
+      id: number;
+      start_date: string;
+      end_date?: string | undefined;
+      difference?: number | undefined;
+    };
+    user: {
+      id: number;
+      email: string;
+      first_name: string;
+      last_name: string;
+      team_id: number;
+      team_admin: boolean;
+      user_status: string;
+      full_name: string;
+      initials: string;
+    };
+  },
+  index: number,
+  array: {
+    id: number;
+    due_date: string;
+    estimated_time_hours: number;
+    estimated_time_minutes: number;
+    total_spent_time: string;
+    notes: string;
+    status: string;
+    progress: number;
+    extra_data: string;
+    project: { id: number; name: string; deletable: boolean };
+    task_category: { id: number; name: string; deletable: boolean };
+    first_log: {
+      id: number;
+      start_date: string;
+      end_date?: string | undefined;
+      difference?: number | undefined;
+    };
+    last_log: {
+      id: number;
+      start_date: string;
+      end_date?: string | undefined;
+      difference?: number | undefined;
+    };
+    user: {
+      id: number;
+      email: string;
+      first_name: string;
+      last_name: string;
+      team_id: number;
+      team_admin: boolean;
+      user_status: string;
+      full_name: string;
+      initials: string;
+    };
+  }[]
+): unknown {
+  throw new Error('Function not implemented.');
+}
--- src/components/ButtonSection/ButtonSection.vue
+++ src/components/ButtonSection/ButtonSection.vue
@@ -11,33 +11,82 @@
           <v-icon icon="mdi-arrow-right-top" size="small"></v-icon>
           <div class="create-shortcut">Create Shortcut</div>
         </div>
-        <div class="create-shortcut-form"></div>
+        <div class="create-shortcut-form">
+          <v-select
+            v-model="selectedProject"
+            id="selcetProject"
+            label="Select Project"
+            required
+            class="form-item"
+            :items="projects"
+          >
+            <option v-for="option in projects" :value="option">
+              {{ option }}
+            </option>
+          </v-select>
+
+          <v-select
+            v-model="selectedCategory"
+            id="selcetCategory"
+            label="Select Category"
+            :style="{ display: selectDisplay }"
+            required
+            class="form-item"
+            :items="categories"
+          >
+            <option v-for="option in projects" :value="option">
+              {{ option }}
+            </option>
+          </v-select>
+          <div class="">
+            <v-btn
+              color="success"
+              class="form-item"
+              :disabled="isCategorySelected"
+              @click="createNewShortcut()"
+            >
+              <v-icon class="white" icon="mdi-pencil"></v-icon>
+              Create
+            </v-btn>
+          </div>
+        </div>
       </div>
     </v-dialog>
     <MainButton
       icon="mdi-checkbox-marked"
       buttonName="SHOW ALL TASKS"
       :styles="{ color: 'white', backgroundColor: '#1976d2' }"
-      @click="allTasksDialog = true"
+      @click="getAllTasks"
     />
     <v-dialog v-model="allTasksDialog" width="auto" center>
       <div class="all-tasks-overlay">
         <div class="overlay-title">
-          <div>
+          <div class="left-side">
             <v-icon icon="mdi-laptop" size="small"></v-icon>
             <div class="all-tasks">All Tasks</div>
           </div>
           <div>
-            <v-icon icon="mdi-close" size="small"></v-icon>
+            <v-icon
+              icon="mdi-close"
+              size="small"
+              @click="allTasksDialog = false"
+            ></v-icon>
           </div>
         </div>
         <div class="tasks-list">
-          <TaskCard />
-          <TaskCard />
-          <TaskCard />
-          <TaskCard />
-          <TaskCard />
-          <TaskCard />
+          <TaskCard
+            v-for="item in allTasks"
+            :TaskDetails="item"
+            :key="item.id"
+          />
+          <div class="show-more-bottom">
+            <MainButton
+              icon="mdi-chevron-down"
+              buttonName="SHOW MORE"
+              :styles="{ color: 'white', backgroundColor: '#1976d2' }"
+              @click="getAllTasks"
+            />
+          </div>
         </div>
       </div>
     </v-dialog>
@@ -45,6 +94,7 @@
       icon="mdi-stop"
       buttonName="END ALL TASKS"
       :styles="{ color: 'white', backgroundColor: '#1976d2' }"
+      @click="endAllTasks"
     />
     <div class="text-center">
       <v-menu open-on-hover>
@@ -69,4 +119,4 @@
 
 <script lang="ts" src="./ButtonSection.ts"></script>
 
-<style lang="scss" src="./ButtonSection.scss" scoped></style>
+<style scoped lang="scss" src="./ButtonSection.scss"></style>
